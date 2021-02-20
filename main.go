8a276536782108d6a1c3b7069371dc60a2d21607
“Anonna_Ferdaus” <anonnatahsin132@gmail.com>
2021-02-20 16:00:03 +0600

    First commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//example-1
+func add(a int, b int) int{
+//body
+
+   x := a+b
+   return x
+
+} 
+
+func main() {
+
+   y := add(10, 15)
+
+fmt.Println(y)
+}
+
+
+
+
